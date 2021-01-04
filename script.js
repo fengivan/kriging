0d1bf7f0676db250a7735fe6a6523ac110fabf2c
Ivan <fenivan@gmail.com>
2021-01-04 14:31:49 -0600

    style updates and minev grid

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ let cc=1800;                                                //exponential covari
 let ca=300;                                                 //exponential covariance parameter
 let table=document.querySelector(".input");                 
 let mouseguide=document.querySelector(".mousepos");         
-let grid = document.querySelector(".grid");                 
+let grid = document.querySelector(".grid");     
+let grid2=document.querySelector(".minev");            
 grid.addEventListener("click", recordpoint);                
-grid.addEventListener("mousemove",updatepos);               
+grid.addEventListener("mousemove",updatepos);  
+grid2.addEventListener("mousemove",updatepos2);
+window.addEventListener("keydown", update);             
 let krigged=false;                                          //flag for if showing krigged value at mousepos is on or off
 implementbuttons();                                                 
+
+function update(e)                                          //allows for updating on enter(can change a and c, then update the grid using previous points)
+{
+    if(e.keyCode==13)
+        run();
+}
+
 
 function getrange(data)                                     //gets the min and max of the input values (for color gradient generation)
 {
@@ -29,21 +39,52 @@ function fillgrid()                                         //fills the grid wit
     let gridboxes=40;
     let smallest=getrange(input)[0];
     let biggest=getrange(input)[1];
-    cleargrid();
+    cleargrid(grid);
+    cleargrid(grid2);
     var rect=grid.getBoundingClientRect();
+    var rect2=grid2.getBoundingClientRect();
+    let minev=[];
     for(var i =0; i<gridboxes; i++)
     {
         for(var j=0; j<gridboxes; j++)
         {   
             var x = i*800/gridboxes; 
             var y = j*800/gridboxes;
-            let boxcolor=(1-(krig(x+400/gridboxes, 800-y+400/gridboxes, input, cc, ca)-smallest)/(biggest-smallest))*255;
+            let krigresult=krig(x+400/gridboxes, 800-y+400/gridboxes, input, cc, ca);
+            let boxcolor=(1-(krigresult[0]-smallest)/(biggest-smallest))*255;
             var sq=document.createElement("div");
             sq.className="box";
             sq.style.left=x+"px";
             sq.style.top=y+"px";
-            sq.style.backgroundColor="rgb(255,"+boxcolor+","+boxcolor+")";
+            if(boxcolor>255/2)
+            {
+                boxcolor=(255-boxcolor)*2;
+                sq.style.backgroundColor="rgb("+boxcolor+","+boxcolor+",255)";
+            }
+            else{
+                boxcolor=boxcolor*2;
+                sq.style.backgroundColor="rgb(255,"+boxcolor+","+boxcolor+")";
+            }
             grid.appendChild(sq);
+            minev.push(krigresult[1]);
+        }
+    }
+    smallest=Math.min(...minev);
+    biggest=Math.max(...minev);
+
+    for(var i =0; i<gridboxes; i++)
+    {
+        for(var j=0; j<gridboxes; j++)
+        {   
+            var x = i*800/gridboxes; 
+            var y = j*800/gridboxes;
+            let boxcolor=((minev[i*gridboxes+j]-smallest)/(biggest-smallest))*255;
+            var sq=document.createElement("div");
+            sq.className="box";
+            sq.style.left=x+"px";
+            sq.style.top=y+"px";
+            sq.style.backgroundColor="rgb("+boxcolor+","+boxcolor+","+boxcolor+")";
+            grid2.appendChild(sq);
         }
     }
 }
@@ -54,7 +95,7 @@ function implementbuttons()                                 //implements color a
     document.querySelector(".krig").addEventListener("click", run);
 }
 
-function krig(x, y, data, covariancec, covariancea)         //returns the kriged value at x,y, given a set of x-y points and the values at those x-y points
+function krig(x, y, data, covariancec, covariancea)         //returns the kriged value and minev at x,y, given a set of x-y points and the values at those x-y points
 {
     let matrix=[];
     let lastrow=[];
@@ -75,11 +116,15 @@ function krig(x, y, data, covariancec, covariancea)         //returns the kriged
     vals.push(1);
     matrix.push(lastrow);
     let results=math.multiply(math.inv(matrix), vals);
-    let output=0;
+    let expectedval=0;
+    let error=0;
     for(let i =0; i<results.length-1; i++)
-        output+=results[i]*data[i][2];
-    
-    return output;
+        {
+            expectedval+=results[i]*data[i][2];
+            error+=results[i]*vals[i];
+        }
+    error=covariancec-results[results.length-1]-error;
+    return [expectedval, error];
 }
 
 function clear()                                            //clears everything on the grid and in the table
@@ -87,16 +132,17 @@ function clear()                                            //clears everything 
     krigged=false;
     points=0;
     input=[];
-    cleargrid();
+    cleargrid(grid);
+    cleargrid(grid2);
     mouseguide.rows[1].cells[2].innerHTML=0;
     while(table.childNodes.length>2)
         table.removeChild(table.lastChild);
 }
 
-function cleargrid()                                        //clears the gird
+function cleargrid(g)                                        //clears the gird
 {
-    while(grid.childNodes.length>0)
-        grid.removeChild(grid.lastChild);
+    while(g.childNodes.length>0)
+        g.removeChild(g.lastChild);
     
 }
 
@@ -108,14 +154,32 @@ function updatepos(e)                                       //updates the mouse 
     mouseguide.rows[1].cells[0].innerHTML=x;
     mouseguide.rows[1].cells[1].innerHTML=800-y;
     if(krigged)
-        mouseguide.rows[1].cells[2].innerHTML=Math.round(krig(x, 800-y, input, cc, ca)*10000)/10000;
-
+    {
+        let kresult=krig(x, 800-y, input, cc, ca);
+        mouseguide.rows[1].cells[2].innerHTML=Math.round(kresult[0]*10000)/10000;
+        mouseguide.rows[1].cells[3].innerHTML=Math.round(kresult[1]*10000)/10000;
+    }
+}
+function updatepos2(e)                                       //updates the mouse position indiator on mouse movement within the grid2
+{
+    var rect = grid2.getBoundingClientRect();
+    var x = e.clientX - rect.left; 
+    var y = e.clientY - rect.top;  
+    mouseguide.rows[1].cells[0].innerHTML=x;
+    mouseguide.rows[1].cells[1].innerHTML=800-y;
+    if(krigged)
+    {
+        let kresult=krig(x, 800-y, input, cc, ca);
+        mouseguide.rows[1].cells[2].innerHTML=Math.round(kresult[0]*10000)/10000;
+        mouseguide.rows[1].cells[3].innerHTML=Math.round(kresult[1]*10000)/10000;
+    }
 }
 
 function run()                                              //collects all the data from the table, and calls fillgrid. also sets boolean flag
 {
     cc=document.querySelector(".c").value;
     ca=document.querySelector(".a").value;
+    input=[];
     for(var i=1; i<points+1; i++)
         input.push([table.rows[i].cells[0].innerHTML,table.rows[i].cells[1].innerHTML,table.rows[i].cells[2].children[0].value]);
     fillgrid();
